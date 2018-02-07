4af6b315d638ce0027e2b9f7831c908264f3be16
RhuanFelipe <rhuanfelsilva@gmail.com>
2018-02-07 17:52:51 -0300

    first commit

---
+++ application/views/sobre.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Página Inicial</title>	
+</head>
+<body>
+	<div id="container">
+		<h1>Site Codeigniter!</h1>
+		<div id="body">
+			<p>sobre a empresa</p>
+		</div>
+	</div>
+</body>
+</html>
