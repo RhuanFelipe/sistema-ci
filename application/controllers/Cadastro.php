309bdd7a757ebaf946c86d148198819ee05672a0
RhuanFelipe <rhuanfelsilva@gmail.com>
2018-03-19 16:19:07 -0300

    renderizar imagem

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -28,7 +28,6 @@ class Cadastro extends CI_Controller {
 			
 			$this->form_validation->set_rules('filme', 'Filme', 'required');
 			$this->form_validation->set_rules('descricao', 'Descrição', 'required');
-
 			$this->load->library('upload');
 			$this->upload->setName($novo_nome);
 			$this->upload->setTypes('jpeg|png|jpg');
@@ -48,14 +47,29 @@ class Cadastro extends CI_Controller {
 			}else{
 				$this->load->model('Filmes');
                 $data_upload = $this->upload->data();
-				$attributes = array(
-					"nome_filme" => $filme,
-					"desc_filme" => $descricao,
-					"foto_filme" => 'public/arquivos/'.$data_upload['file_name']
-				);
+       			/* faz resize da foto */
+                $this->load->library('image_lib');
+                $this->image_lib->setPath($_FILES['userfile']['tmp_name']);
+                $this->image_lib->setNew($data_upload['file_path'] . 'thumbs/' . $data_upload['file_name']);
+              	$this->image_lib->setLargura(200);
+                $this->image_lib->setAltura(150);
+                $config_resize = $this->image_lib->setConfiguration();
+                $this->image_lib->initialize($config_resize);
+
+               	if (!$this->image_lib->resize()) {
+                    unlink($data_upload['full_path']);
+                    echo $this->image_lib->display_errors();
+                } else {
+					$attributes = array(
+						"nome_filme" => $filme,
+						"desc_filme" => $descricao,
+						"foto_filme" => 'public/arquivos/'.$data_upload['file_name'],
+	                    "filme_thumb" => 'public/arquivos/thumbs/' . $data_upload['file_name']
+					);
 				
 				$this->Filmes->cadastrar_filmes($attributes);
 				$data['sucesso'] = 'Imagem salva com sucesso!';
+				}
 			}
           }
 			
